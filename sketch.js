5d4b1e6ac560442f028334fb0f5383e0b3e31694
Kerlly Zuin <kerlly152@gmail.com>
2024-01-18 13:43:33 -0500

    Ingreso de datos (tiles)

--- sketch.js
+++ sketch.js
@@ -1,3 +1,163 @@
+const celdas = [];
+const RETICULA = 4;
+
+const azulejoz = [];
+const NA = 5;
+const reglas = [
+  {
+    // tile 0 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 1 
+    UP: 0, 
+    RIGHT: 1,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 2
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 3
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 4 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 5 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 6 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 7 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 8
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 9
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 10 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 11 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 12 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 13 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 14 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 15 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 16
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 17
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+  {
+    // tile 18 
+    UP: 0, 
+    RIGHT: 0,
+    DOWN: 0, 
+    LEFT: 0, 
+  },
+
+];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
